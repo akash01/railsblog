79cbe5f26592220ab4c2a81134a183ecc027ceb2
mukesh <pace.akash@gmail.com>
2014-01-31 15:47:17 +0200

    refactored files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   end
 
   def show
-  	@user = User.find(params[:id])
+    redirect_to :admin
+  	#@user = User.find(params[:id])
   end
 
   def new
